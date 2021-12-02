89e24295e5cc4ee5aab05b621212f58aa7f95664
SuperMariia <mvp709@gmail.com>
2021-12-02 19:07:31 +0300

    second commit

--- app/src/main/java/com/Mariia/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/Mariia/myfirstapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.Mariia.myfirstapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+lateinit var plusButton: Button
+lateinit var minusButton: Button
+lateinit var multiplicationButton: Button
+lateinit var historyButton: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        plusButton = findViewById(R.id.plus_button)
+        plusButton.setOnClickListener { }
+
+
+        historyButton = findViewById(R.id.history_button)
+        historyButton.setOnClickListener {
+            val secondActivityIntent: Intent = Intent(this, SecondActivityHistory::class.java)
+            startActivity(secondActivityIntent)
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/Mariia/myfirstapp/SecondActivityHistory.kt
@@ -0,0 +1,11 @@
+package com.Mariia.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivityHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_history)
+    }
+}
